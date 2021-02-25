9cece516bfe7f1aaba3c9de64ad85ea1202180a3
Adelinanica <adelinanica29@yahoo.com>
2021-02-25 20:44:26 +0200

    create html content and json

---
+++ functions.js
@@ -0,0 +1,16 @@
+let allMembers=[];
+
+function showMembers(members){
+  allMembersHtml = skills.map
+}
+
+
+fetch("teams.json")
+.then( response =>  response.json() )
+.then( teams => {console.info(teams.json)})
+
+
+
+
+
+
